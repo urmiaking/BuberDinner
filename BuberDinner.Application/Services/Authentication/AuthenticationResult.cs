13e464b30d903cdb8bf0c555848f64e641695717
Masoud - Home <masoud.brilliant@hotmail.com>
2023-11-04 00:46:08 +0330

    Add user repository

--- BuberDinner.Application/Services/Authentication/AuthenticationResult.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationResult.cs
@@ -2,12 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuberDinner.Domain.Entities;
 
 namespace BuberDinner.Application.Services.Authentication;
 
 public record AuthenticationResult(
-    Guid Id,
-    string FirstName,
-    string LastName,
-    string Email,
+    User User,
     string Token);
---
+++ BuberDinner.Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Entities;
+
+namespace BuberDinner.Infrastructure.Persistence;
+
+public class UserRepository : IUserRepository
+{
+    private static readonly List<User> Users = new ();
+
+    public User? GetUserByEmail(string email)
+    {
+        return Users.SingleOrDefault(u => u.Email == email);
+    }
+
+    public void Add(User user)
+    {
+        Users.Add(user);
+    }
+}
