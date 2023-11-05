29ddf50bbdfd1662c6def05f2e5b1da05686c571
Masoud - Home <masoud.brilliant@hotmail.com>
2023-11-05 23:11:18 +0330

    Apply exceptions

---
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        return Problem(statusCode: 400, title: exception?.Message);
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,17 +1,25 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddInfrastructure(builder.Configuration)
         .AddApplication();
-        
+
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+    //builder.Services.AddControllers(options => { options.Filters.Add<ErrorHandlingFilterAttribute>();});
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
